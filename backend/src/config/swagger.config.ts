26e8372fd0b5908e57e5732a14d8a6057e0c867d
Mohammad Reza Akhshabi <MoReza.Dev@gmail.com>
2025-09-18 20:20:40 +0330

    backend implemented, working on data models

---
+++ backend/src/config/swagger.config.ts
@@ -0,0 +1,8 @@
+import { DocumentBuilder } from '@nestjs/swagger';
+
+export const swaggerConfig = new DocumentBuilder()
+  .setTitle('فروشگاه آنلاین')
+  .setDescription('بک اند و اندپوینت های فروشگاه آنلاین')
+  .setVersion('1.0')
+  .addTag('User')
+  .build();
